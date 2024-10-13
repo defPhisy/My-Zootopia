52ce9f9a7715fe7e4f628b6a94c2f08b3ec92cbd
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-10-13 11:41:11 +0200

    bring animal data into html

---
+++ animals_web_generator.py
@@ -0,0 +1,38 @@
+import json
+
+
+def main():
+    # read json and html template
+    with open("animals_data.json", "r") as handle:
+        json_data = json.loads(handle.read())
+
+    with open("animals_template.html", "r") as handle:
+        html_template = handle.read()
+
+    # get animal specs as string
+    animal_specs: str = get_animal_specs(json_data)
+    # replace placeholder in html template with new animal specs
+    new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_specs)
+
+    # write modified html template to new file
+    with open("animals.html", "w") as handle:
+        handle.write(new_html)
+
+
+def get_animal_specs(json: dict) -> str:
+    output = ""
+    for animal in json:
+        name = animal["name"]
+        diet = animal["characteristics"]["diet"]
+        location = animal["locations"][0]
+        animal_type = animal["characteristics"].get("type")
+        output += (
+            f"Name: {name}\n" + f"Diet: {diet}\n" + f"Location: {location}\n"
+        )
+        if animal_type:
+            output += f"Type: {animal_type.capitalize()}\n"
+    return output
+
+
+if __name__ == "__main__":
+    main()
