dda238be4e5d10266b7665f7cdc70bc635aa4e92
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-10-17 14:49:15 +0200

    improve error handling

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,19 +1,29 @@
 from data_fetcher import get_animals
+import json
 
 
 def main():
     # read json and html template
-    json_data = get_animals()
-
     with open("animals_template.html", "r") as handle:
         html_template = handle.read()
 
-    animal_specs = get_animal_specs(json_data)
+    json_data, message = get_animals()
+    if not json_data:
+        message = get_html_message(message)
+        new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", message)
 
-    # replace placeholder in html template with new animal specs
-    new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_specs)
+    else:
+        animal_specs = get_animal_specs(json_data)
+        # replace placeholder in html template with new animal specs
+        new_html = html_template.replace(
+            "__REPLACE_ANIMALS_INFO__", animal_specs
+        )
 
     # write modified html template to new file
+    save_file(new_html)
+
+
+def save_file(new_html):
     with open("animals.html", "w") as handle:
         try:
             handle.write(new_html)
@@ -25,7 +35,7 @@ def main():
             )
 
 
-def get_animal_specs(json: list) -> str:
+def get_animal_specs(json) -> str:
     output = ""
     for animal in json:
         name = animal["name"]
@@ -51,6 +61,16 @@ def get_html_serialization(name, diet, location, animal_type):
     output += f"<li><strong>Diet:</strong> {diet}</li></ul></div></li>"
 
     return output
+
+
+def get_html_message(message: str) -> str:
+    if "Error" in message[0]:
+        output = json.loads(message[-1])
+        output = output["error"].capitalize()
+    else:
+        output = message
+
+    return f"<h2>{output}</h2>"
 
 
 if __name__ == "__main__":
--- data_fetcher.py
+++ data_fetcher.py
@@ -17,9 +17,14 @@ def fetch_animals(animal: str):
     url = "https://api.api-ninjas.com/v1/animals"
     headers = {"X-Api-Key": API_KEY, "name": animal}
     response = requests.get(url, headers)
-    if not response:
-        return "No animals found"
+    if not response.json():
+        data = ""
+        message = f"No animals found with '{animal}'"
     elif response.status_code == requests.codes.ok:
-        return response.json()
+        data = response.json()
+        message = response.status_code
     else:
-        return "Error:", response.status_code, response.text
+        data = ""
+        message = "Error:", response.status_code, response.text
+
+    return data, message
